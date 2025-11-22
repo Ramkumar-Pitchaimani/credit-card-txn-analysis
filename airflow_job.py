df66f1fc38408aafc1b50e60910cf44a20838653
Ramkumar <p.raamkumar@gmail.com>
2025-11-22 14:00:30 +0530

    retry

--- airflow_job.py
+++ airflow_job.py
@@ -10,7 +10,7 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=1),
     'start_date': datetime(2025, 5, 30),
 }
 
